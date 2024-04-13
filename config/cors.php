b65eebc7e5be4fa3deb454acb11b9c13b66b346c
dian afrial <agil.rahadi@gmail.com>
2024-04-13 14:19:27 +0700

    updatae cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://www.pinkfam.biz.id', 'https://pinkfam.biz.id'],
 
     'allowed_origins_patterns' => [],
 
